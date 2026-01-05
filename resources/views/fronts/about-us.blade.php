d2da01cd3a67af0c90824fe9e85ee964376d6567
Abish 80 <abhic1915@gmail.com>
2026-01-05 15:18:12 +0530

    update

--- resources/views/fronts/about-us.blade.php
+++ resources/views/fronts/about-us.blade.php
@@ -3,16 +3,70 @@
 <div class="container">
     <div class="hero-about">
         <div class="about-image">
-            <img src="public/admin/images/award.jpg"
-                alt="GENERAL MANOJ PANDE, PVSM, AVSM, VSM, ADC">
+            <img src="public/admin/images/award.jpg" alt="GENERAL MANOJ PANDE, PVSM, AVSM, VSM, ADC">
         </div>
         <h3 class="title mt-0 mb-10 text-uppercase text-center">
             <span class="cosa-msg">History</span>
         </h3>
         <div class="dis-message">
-            <p>Under the authority of headquarter 21 sub area SOP no. 2010/A4 date 24 March2017, Veteran Sahayta Kendra was established at station headquarters Dharamshala with effect from 17July2017, for registration of veteran and also addressing issues related to welfare of ESM/Widow/Virnari including girvensiros, pension anomalies and other issues!</p>
+            <p>Under the authority of headquarter 21 sub area SOP no. 2010/A4 date 24 March2017, Veteran Sahayta Kendra
+                was established at station headquarters Dharamshala with effect from 17July2017, for registration of
+                veteran and also addressing issues related to welfare of ESM/Widow/Virnari including girvensiros,
+                pension anomalies and other issues!</p>
         </div>
 
+
+        <div class="org-chart">
+
+            <div class="org-main-heading">
+                <h2>Organization</h2>
+            </div>
+            <div class="org-box">Station Commander</div>
+            <div class="arrow">↓</div>
+
+            <div class="org-box">Adam Commended</div>
+            <div class="arrow">↓</div>
+
+            <div class="org-box">
+                HQ Section <br>
+                <span class="org-sub">(OIC ECHS Cell – SSO)</span>
+            </div>
+
+            <div class="arrow">↓</div>
+            <div class="split">
+                <div>
+                    <div class="arrow">↓</div>
+                    <div class="org-box">ECHS Cell</div>
+                </div>
+
+                <div>
+                    <div class="arrow">↓</div>
+                    <div class="org-box">VSK Cell</div>
+                </div>
+            </div>
+
+            <div class="arrow">↓</div>
+
+            <div class="org-box">Q Section</div>
+            <div class="arrow">↓</div>
+
+            <div class="org-box">Accounts Section</div>
+            <div class="arrow">↓</div>
+
+            <div class="org-box">Store Section</div>
+
+        </div>
+
+        <div class="content-detail">
+            <h2 class="content-heading">Content Us</h2>
+
+            <div class="location-details">
+                <p><strong>Location:</strong> Veteran Sahayta Kendra, Station Headquarters, Dharamshala, HP-176215</p>
+                <p><strong>Helpline No:</strong> 01892-224330 (OIC VSK)</p>
+                <p><strong>Landline No:</strong> 01892-224330 (OIC VSK)</p>
+                <p><strong>Email ID:</strong> <a href="mailto:echscelldharamshala@gmail.com">echscelldharamshala@gmail.com</a></p>
+            </div>
+        </div>
     </div>
 </div>
 
--- resources/views/fronts/funeral.blade.php
+++ resources/views/fronts/funeral.blade.php
@@ -30,21 +30,24 @@
 
 
 <!-- PAGE TITLE -->
-<h2>ADLRS/Demise Grant and Military Funeral</h2>
+
 
 <div class="tab-content mt-4" id="mainTabsContent">
 
     <!-- ADLRS TAB -->
     <div class="tab-pane fade show active" id="adlrs" role="tabpanel" aria-labelledby="adlrs-tab">
+        <h2>ADLRS</h2>
         <div class="main-adlrs">
             <span class="adlrs-content">
                 As per Army Headquarter letter number B/45322/AG/ADLRS/R&W-4 dated 8 july 2016. On behalf of (COAS)
                 ADLRs
                 ceremony is being organised and RS; 10000 is being paid to NOK on death of EMS. The following document
-                to be
-                produced are grant of ADLRs: Copy of discharge Book 2. Copy of PPO 3. Copy of Death certificate 4. Copy
-                of
-                Addhar card 5. Copy of bankers details of NOK 6. copy of CSD cards both (Liq and Gross)
+                the detailed is as under. to be
+                produced are grant of ADLRs: <strong> <br> 1. Copy of discharge Book <br> 2. Copy of PPO <br> 3. Copy of
+                    Death certificate <br> 4. Copy
+                    of
+                    Addhar card <br> 5. Copy of bankers details of NOK <br> 6. copy of CSD cards both (Liq and Gross)
+                </strong>
             </span>
         </div>
         <!-- TABLE SECTION -->
@@ -70,7 +73,7 @@
                 <thead>
                     <tr>
                         <th>Sr.No</th>
-                        <th>Current year</th>
+                        <th>Year</th>
                         <th>NOK</th>
                         <th>Number of Amount</th>
                     </tr>
@@ -79,24 +82,81 @@
                 <tbody>
                     <tr>
                         <td>1.</td>
-                        <td>2025-26</td>
-                        <td>103</td>
-                        <td>10,30,000</td>
+                        <td>Current year</td>
+                        <td>114</td>
+                        <td>11,40,000</td>
                     </tr>
                 </tbody>
             </table>
 
         </div>
+
+        <div class="container">
+            <div class="org-chart">
+
+                <div class="org-main-heading">
+                    <h2>Organization</h2>
+                </div>
+                <div class="org-box">Station Commander</div>
+                <div class="arrow">↓</div>
+
+                <div class="org-box">Canteen manager</div>
+                <div class="arrow">↓</div>
+
+                <div class="org-box">
+                    Wellfare officer
+                </div>
+                <div class="arrow">↓</div>
+
+                <div class="org-box">field officer(not depended on Veteran pollution) </div>
+                <div class="arrow">↓</div>
+
+                <div class="org-box">document required for the grant adlrs is as under</div>
+                <div class="arrow">↓</div>
+                <div class="split">
+                    
+            <div class="org-box org-box-1">
+                <p><strong>ESM ISSUES TO BE DISCUSSED DURING INTERACTION</strong></p>
+
+                <a href="{{ url('public/admin/images/Issues_to_be_Discussed_Form.pdf') }}"
+                   target="_blank">
+                    <button type="button" class="btn btn-primary view-pdf-01">
+                        View / Download PDF
+                    </button>
+                </a>
+            </div>
+
+            <div class="org-box org-box-1">
+                <p><strong>FORM FOR GRIEVANCE / COMPLAINT BY ESM OR DEPENDENT OF ESM</strong></p>
+
+                <a href="{{ url('public/admin/images/Grievance_Complaint_Form.pdf') }}"
+                   target="_blank">
+                    <button type="button" class="btn btn-primary view-pdf-01">
+                        View / Download PDF
+                    </button>
+                </a>
+            </div>
+
+                </div>
+
+            </div>
+        </div>
+
+
     </div>
 
     <!-- DEMISE TAB -->
     <div class="tab-pane fade" id="demise" role="tabpanel" aria-labelledby="demise-tab">
 
+    <h2>Demise Grant</h2>
+
         <div class="main-adlrs">
             <span class="adlrs-content">
-                The following document are required for payment of demised Grant: 1. Personal Application 2.Copy of
-                discharge Book 3. Copy of PPO 4. Copy of Death certificate 5. Copy of Addhar card 6. Copy of bankers
-                details of NOK.
+                The following document are required for payment of demised Grant: <strong> <br> 1. Personal Application
+                    <br> 2.Copy of
+                    discharge Book <br> 3. Copy of PPO <br> 4. Copy of Death certificate <br> 5. Copy of Addhar card
+                    <br> 6. Copy of bankers
+                    details of NOK. </strong>
             </span>
         </div>
         <!-- TABLE SECTION -->
@@ -132,8 +192,8 @@
                     <tr>
                         <td>1.</td>
                         <td>2025-26</td>
-                        <td>207</td>
-                        <td>14,28,000</td>
+                        <td>150</td>
+                        <td>17,50,000</td>
                     </tr>
                 </tbody>
             </table>
@@ -144,6 +204,7 @@
     <!-- MILITARY TAB -->
     <div class="funeral-gallery">
         <div class="tab-pane fade" id="military" role="tabpanel" aria-labelledby="military-tab">
+            <h2>Military Funeral</h2>
             <div class="row g-3">
                 <div class="col-md-4 col-6"><img src="public/admin/images/funeral-1.jpeg" class="img-fluid rounded">
                 </div>
