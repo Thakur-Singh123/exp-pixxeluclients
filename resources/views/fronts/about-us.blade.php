a179c5030d128e98fca1ab00fdd15d8e39e712f7
Abish 80 <abhic1915@gmail.com>
2026-01-20 15:49:46 +0530

    update

--- resources/views/fronts/about-us.blade.php
+++ resources/views/fronts/about-us.blade.php
@@ -71,6 +71,118 @@
 
         </div>
 
+        <div class="container my-4">
+            <div class="row justify-content-center">
+                <div class="col-lg-12 col-md-11 col-10 bg-white border p-4">
+
+                    <!-- TITLE -->
+                    <h5 class="text-center fw-bold text-uppercase mb-3 fs-2">
+                        Area of STN HQ Dharamsala
+                    </h5>
+
+                    <!-- SUB TITLE -->
+                    <p class="text-center small fw-semibold mb-4 fs-4">
+                        Kangra Tehsil Map
+                    </p>
+
+                    <!-- MAP IMAGE -->
+                    <div class="text-center mb-4">
+                        <img src="{{ asset('public/admin/images/kangra_map.jpeg') }}"
+                            alt="Area of STN HQ Dharamsala Map" class="img-fluid border">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container my-4">
+            <div class="row justify-content-center">
+                <div class="col-lg-12 col-md-10 col-8 bg-white border p-4">
+
+                    <!-- TITLE -->
+                    <h6 class="text-center fw-bold text-uppercase mb-1 fs-2">
+                        Dependency State: STN HQ Dharamsala
+                    </h6>
+
+                    <h6 class="text-center fw-bold text-uppercase text-decoration-underline mb-4 fs-4">
+                        Summary
+                    </h6>
+
+                    <!-- TABLE -->
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-sm align-middle text-center fs-6">
+                            <thead>
+                                <tr>
+                                    <th>Ser No</th>
+                                    <th>Name of Tehsil</th>
+                                    <th>No of ESM</th>
+                                    <th>No of Widows</th>
+                                    <th>No of Veer Naris</th>
+                                    <th>Dependents</th>
+                                    <th>Total</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td class="text-start">Shahpur (Distt-Kangra)</td>
+                                    <td>1849</td>
+                                    <td>561</td>
+                                    <td>08</td>
+                                    <td>2389</td>
+                                    <td>4807</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td class="text-start">Jawali</td>
+                                    <td>1407</td>
+                                    <td>314</td>
+                                    <td>07</td>
+                                    <td>1398</td>
+                                    <td>3126</td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td class="text-start">Harchakian</td>
+                                    <td>361</td>
+                                    <td>62</td>
+                                    <td>03</td>
+                                    <td>498</td>
+                                    <td>924</td>
+                                </tr>
+                                <tr>
+                                    <td>4</td>
+                                    <td class="text-start">Dharamsala</td>
+                                    <td>262</td>
+                                    <td>116</td>
+                                    <td>03</td>
+                                    <td>203</td>
+                                    <td>584</td>
+                                </tr>
+                                <tr>
+                                    <td>5</td>
+                                    <td class="text-start">Shunta (Distt-Chamba)</td>
+                                    <td>438</td>
+                                    <td>142</td>
+                                    <td>06</td>
+                                    <td>787</td>
+                                    <td>1373</td>
+                                </tr>
+                                <tr class="fw-bold">
+                                    <td colspan="2">Grand Total</td>
+                                    <td>4317</td>
+                                    <td>1195</td>
+                                    <td>27</td>
+                                    <td>5275</td>
+                                    <td>10814</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
         <div class="content-detail">
             <h2 class="content-heading">Contact Us</h2>
 
@@ -78,7 +190,8 @@
                 <p><strong>Location:</strong> Veteran Sahayta Kendra, Station Headquarters, Dharamshala, HP-176215</p>
                 <p><strong>Helpline No:</strong> 7834023404 </p>
                 <p><strong>Landline No:</strong> 01892-220216 </p>
-                <p><strong>Email ID:</strong> <a href="mailto:echscelldharamshala@gmail.com">echscelldharamshala@gmail.com</a></p>
+                <p><strong>Email ID:</strong> <a
+                        href="mailto:echscelldharamshala@gmail.com">echscelldharamshala@gmail.com</a></p>
             </div>
         </div>
     </div>
